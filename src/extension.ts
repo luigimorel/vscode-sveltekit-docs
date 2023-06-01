b00c3429edf4366b7ff29f5a4e699a935fb54c02
Luigi Morel <morelmiles@gmail.com>
2023-06-01 16:41:23 +0300

    fix: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
     let svelteKitDocsCreatingAProject = vscode.commands.registerCommand('extension.svelteKitDocsCreatingAProject', () => {
-        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://kit.svelte.dev/docs/creating-a-project'));
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://kit.svelte.dev/docscreating-a-project'));
     });
     context.subscriptions.push(svelteKitDocsCreatingAProject);
 
